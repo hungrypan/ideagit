d873177473fe2c26c0c7bb10201e4fb9463d6b35
hungrypan <1602944312@qq.com>
2018-01-18 20:05:35 +0800

    init project

---
+++ src/main/java/com/example/controller/HelloServlet.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("hello servlet");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
